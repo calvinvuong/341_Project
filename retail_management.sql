0a70c8282a633ea11289a8b372b2c80c0c29eef7
Calvin <calvinvuong425@gmail.com>
2018-11-28 17:48:35 -0500

    restored retail_management sql file

--- retail_management.sql
+++ retail_management.sql
@@ -16,6 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `allowed`
+--
+
+DROP TABLE IF EXISTS `allowed`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `allowed` (
+  `employee_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY (`employee_id`,`permission_id`),
+  KEY `permission_id` (`permission_id`),
+  CONSTRAINT `allowed_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`),
+  CONSTRAINT `allowed_ibfk_2` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `allowed`
+--
+
+LOCK TABLES `allowed` WRITE;
+/*!40000 ALTER TABLE `allowed` DISABLE KEYS */;
+/*!40000 ALTER TABLE `allowed` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `bought_items`
 --
 
@@ -25,6 +51,7 @@ DROP TABLE IF EXISTS `bought_items`;
 CREATE TABLE `bought_items` (
   `item_id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
+  `purchase_price` float DEFAULT NULL,
   PRIMARY KEY (`item_id`,`customer_id`),
   KEY `customer_id` (`customer_id`),
   CONSTRAINT `bought_items_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`),
@@ -55,8 +82,10 @@ CREATE TABLE `customer` (
   `street_address` varchar(25) DEFAULT NULL,
   `city` varchar(15) DEFAULT NULL,
   `state` char(2) DEFAULT NULL,
+  `username` varchar(25) DEFAULT NULL,
+  `password` varchar(25) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,6 +94,7 @@ CREATE TABLE `customer` (
 
 LOCK TABLES `customer` WRITE;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES (1,'Bob','666','elm street','atlanta','GA','bobbo','abc123'),(9,'prom','8823','hello street','end','NY','blah','dsf'),(10,'Hield','93042','dsfnksr','sdfk','OH','unique','blah'),(11,'Poof','9038','jkdsf','apodf','AK','poof','jhiu');
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,7 +112,7 @@ CREATE TABLE `employee` (
   `password` varchar(25) DEFAULT NULL,
   `username` varchar(25) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,6 +121,7 @@ CREATE TABLE `employee` (
 
 LOCK TABLES `employee` WRITE;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,'Calvin','manager','','calvin_boss'),(2,'Bim','retail','nani','bibbi'),(3,'TEsting','manager','orange','testingaccount'),(4,'Kraus','manager','apples','boss'),(5,'Charles','manager','darwin','charles'),(6,'Cr','manager','blue','crew');
 /*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -107,7 +138,7 @@ CREATE TABLE `items` (
   `price` float DEFAULT NULL,
   `quantity` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -116,6 +147,7 @@ CREATE TABLE `items` (
 
 LOCK TABLES `items` WRITE;
 /*!40000 ALTER TABLE `items` DISABLE KEYS */;
+INSERT INTO `items` VALUES (1,'Milk',2.05,0),(2,'Corn',1.03,0),(3,'Baloney',5,0),(4,'Chips',1.59,0);
 /*!40000 ALTER TABLE `items` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -131,8 +163,9 @@ CREATE TABLE `location` (
   `street_address` varchar(25) DEFAULT NULL,
   `city` varchar(15) DEFAULT NULL,
   `state` char(2) DEFAULT NULL,
+  `name` varchar(25) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -141,7 +174,31 @@ CREATE TABLE `location` (
 
 LOCK TABLES `location` WRITE;
 /*!40000 ALTER TABLE `location` DISABLE KEYS */;
+INSERT INTO `location` VALUES (1,'close by','Pawnee','IN','Food and Stuff'),(2,'euclid','Cleveland','OH','kenko'),(3,'fields','Cleveland','OH','costco');
 /*!40000 ALTER TABLE `location` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permission`
+--
+
+DROP TABLE IF EXISTS `permission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permission`
+--
+
+LOCK TABLES `permission` WRITE;
+/*!40000 ALTER TABLE `permission` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permission` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -154,6 +211,7 @@ DROP TABLE IF EXISTS `stores`;
 CREATE TABLE `stores` (
   `item_id` int(11) NOT NULL,
   `location_id` int(11) NOT NULL,
+  `quantity` int(11) DEFAULT NULL,
   PRIMARY KEY (`item_id`,`location_id`),
   KEY `location_id` (`location_id`),
   CONSTRAINT `stores_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`),
@@ -167,7 +225,34 @@ CREATE TABLE `stores` (
 
 LOCK TABLES `stores` WRITE;
 /*!40000 ALTER TABLE `stores` DISABLE KEYS */;
+INSERT INTO `stores` VALUES (1,1,10),(1,2,5),(1,3,8),(2,1,10),(2,3,30),(3,1,3),(3,2,5),(3,3,2),(4,1,93),(4,2,34),(4,3,24);
 /*!40000 ALTER TABLE `stores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `works_at`
+--
+
+DROP TABLE IF EXISTS `works_at`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `works_at` (
+  `employee_id` int(11) NOT NULL,
+  `location_id` int(11) NOT NULL,
+  PRIMARY KEY (`employee_id`,`location_id`),
+  KEY `location_id` (`location_id`),
+  CONSTRAINT `works_at_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`),
+  CONSTRAINT `works_at_ibfk_2` FOREIGN KEY (`location_id`) REFERENCES `location` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `works_at`
+--
+
+LOCK TABLES `works_at` WRITE;
+/*!40000 ALTER TABLE `works_at` DISABLE KEYS */;
+/*!40000 ALTER TABLE `works_at` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -179,4 +264,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-28 17:34:09
+-- Dump completed on 2018-11-28 17:47:37
